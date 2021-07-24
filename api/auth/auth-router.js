e7d1ae4aaf3444de95f1b58ae6d0e674c2b49f2e
wzd200 <william.z.dye@gmail.com>
2021-07-24 15:39:19 -0400

    Final touches on initial wiring

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const Users = require('./auth-model')
 
-router.get('/api/auth', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const games = await Users.findBy()
     res.json(games)
