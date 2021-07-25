67ebc10b8b2c4f9d0cf4d9b402f57a3c5c3cb995
wzd200 <william.z.dye@gmail.com>
2021-07-25 10:54:01 -0400

    fixed small error in items router scaffolding

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const Items = require('./items-model')
 
-router.get('/'), async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
         const item = await Items.findItemBy()
         res.json(item)
     } catch (err) {
         next(err)
     }
-}
+})
 
 module.exports = router
