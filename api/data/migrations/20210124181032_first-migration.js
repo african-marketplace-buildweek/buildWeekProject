59b901901962453e8bd870725d16287ea013df8d
wzd200 <william.z.dye@gmail.com>
2021-07-25 18:58:48 -0400

    Created initial tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -4,10 +4,38 @@ exports.up = async (knex) => {
       users.increments('user_id')
       users.string('username', 200).notNullable().unique()
       users.string('password', 200).notNullable()
+      users.string('user_picture')
       users.timestamps(false, true)
+    })
+
+    .createTable('items', (items) => {
+      items.increments('item_id')
+      items.string('item_name', 200).notNullable()
+      items.string('item_category').notNullable()
+      items.integer('item_price').notNullable()
+      items.string('item_description', 300).notNullable()
+    })
+
+    .createTable('users_items', (users_items) => {
+      users_items.increments('users_items_id')
+      users_items.integer('user_id')
+        .unsigned()
+        .references('user_id')
+        .inTable('users')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
+      users_items.integer('item_id')
+        .unsigned()
+        .references('item_id')
+        .inTable('items')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+    .dropTableIfExists('users_items')
+    .dropTableIfExists('items')
+    .dropTableIfExists('users')
 }
