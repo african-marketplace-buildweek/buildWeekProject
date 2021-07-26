cc1b8bac51960cd484f1bf8f1a775a2acbaee05f
wzd200 <william.z.dye@gmail.com>
2021-07-25 20:30:20 -0400

    Fleshed out item router scaffolding

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -1,7 +1,27 @@
-function findItemBy() {
+function findItems() {
     return 'findItemBy wired'
 }
 
+function findItemById() {
+    return 'findItemById wired'
+}
+
+function addItem() {
+    return 'addItem wired'
+}
+
+function updateItem() {
+    return 'updateItem wired'
+}
+
+function deleteItem() {
+    return 'deleteItem wired'
+}
+
 module.exports = {
-    findItemBy
+    findItems,
+    findItemById,
+    addItem,
+    updateItem,
+    deleteItem
 }
--- api/items/items-router.js
+++ api/items/items-router.js
@@ -3,7 +3,43 @@ const Items = require('./items-model')
 
 router.get('/', async (req, res, next) => {
     try {
-        const item = await Items.findItemBy()
+        const item = await Items.findItems()
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const item = await Items.findItemById()
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const item = await Items.addItem()
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const item = await Items.updateItem()
+        res.json(item)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const item = await Items.deleteItem()
         res.json(item)
     } catch (err) {
         next(err)
