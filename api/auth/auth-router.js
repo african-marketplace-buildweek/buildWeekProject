a35b09a8ebe781336cdc873caf00a1e24f79ea6c
wzd200 <william.z.dye@gmail.com>
2021-07-26 11:54:12 -0400

    .

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,26 @@
 const router = require('express').Router()
-const Users = require('./auth-model')
+const Users = require('../users/users-model')
+const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+// const { jwtSecret } = require('../secrets/index')
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, password, user_picture } = req.body
+    const rounds = process.env.BCRYPT_ROUNDS || 8
+    const hash = bcrypt.hashSync(password, rounds)
+    const newUser = {
+      username: username,
+      password: hash,
+      user_picture: user_picture
+    }
+
+    const dbUser = await Users.insertUser(newUser)
+    res.status(201).json(dbUser)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.get('/', async (req, res, next) => {
   try {
