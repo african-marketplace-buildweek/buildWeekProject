0ef04304150b3cc2867fd61346545294fad9f54a
wzd200 <william.z.dye@gmail.com>
2021-07-27 10:55:57 -0400

    Added market_id to user seeds

--- api/data/seeds/04-users.js.js
+++ api/data/seeds/04-users.js.js
@@ -3,12 +3,14 @@ const users = [
   { 
     username: 'harry',
     password: '1234',
-    user_picture: 'https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg'
+    user_picture: 'https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg',
+    market_id: 1
   },
   { 
     username: 'hermione',
     password: '1234',
-    user_picture: 'https://static.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%29.jpg/revision/latest?cb=20141230074301'
+    user_picture: 'https://static.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%29.jpg/revision/latest?cb=20141230074301',
+    market_id: 2
   },
 ]
 
