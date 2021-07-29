85bc3f05245c8989d50df37674f00bada53cac98
wzd200 <william.z.dye@gmail.com>
2021-07-29 13:08:19 -0400

    Added even more seeds

--- api/data/seeds/03-items.js.js
+++ api/data/seeds/03-items.js.js
@@ -70,6 +70,83 @@ const items = [
     item_description: 'Freshly retired',
     market_id: 5
   },
+  { 
+    item_name: 'Goat Meat',
+    item_category: 'Animal Products',
+    item_price: 17,
+    item_description: 'The greatest of all time',
+    market_id: 1
+  },
+  { 
+    item_name: 'Dry Maize',
+    item_category: 'Maize',
+    item_price: 3,
+    item_description: 'Have a glass of water ready',
+    market_id: 1
+  },
+  { 
+    item_name: 'Green Maize',
+    item_category: 'Maize',
+    item_price: 2,
+    item_description: 'It\'s not easy being green',
+    market_id: 2
+  },
+  { 
+    item_name: 'Wheat',
+    item_category: 'Wheat',
+    item_price: .5,
+    item_description: 'It\'s s-wheat ;)',
+    market_id: 2
+  },
+  { 
+    item_name: 'Avocado',
+    item_category: 'Fruits',
+    item_price: 6,
+    item_description: 'Grab yer toast',
+    market_id: 3
+  },
+  { 
+    item_name: 'Apple Bananas',
+    item_category: 'Fruits',
+    item_price: 4.5,
+    item_description: 'Two for the price of one',
+    market_id: 3
+  },
+  { 
+    item_name: 'Unprocessed Vanilla',
+    item_category: 'Animal Products',
+    item_price: 12,
+    item_description: 'Still unfinis-',
+    market_id: 4
+  },
+  { 
+    item_name: 'Sweet Potatoes',
+    item_category: 'Roots & Tubers',
+    item_price: 2.5,
+    item_description: 'They give the nicest compliments',
+    market_id: 4
+  },
+  { 
+    item_name: 'Ground Nuts',
+    item_category: 'Nuts',
+    item_price: 2,
+    item_description: 'They were found on the ground',
+    market_id: 5
+  },
+  { 
+    item_name: 'Onions',
+    item_category: 'Vegetables',
+    item_price: 2.3,
+    item_description: 'Get ready to cry tears of joy',
+    market_id: 5
+  },
+  { 
+    item_name: 'Tomatos',
+    item_category: 'Fruit',
+    item_price: 2,
+    item_description: 'From my head',
+    market_id: 5
+  },
 
 ]
 
